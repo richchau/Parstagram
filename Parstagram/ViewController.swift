3902b38c2656e9946ea1d666774857f25cd0feda
Rich Chau <richchau2022@gmail.com>
2020-03-19 18:02:35 -0700

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by Rich Chau on 3/19/20.
+//  Copyright © 2020 Rich Chau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
